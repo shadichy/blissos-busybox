080d47e11316ada7ef282b568e4c715f13de4272
shadichy <hotrobb360web@gmail.com>
2024-12-23 11:40:24 +0700

    remove explicit handlings of leftover non-building package
    Signed-off-by: shadichy <hotrobb360web@gmail.com>

--- build-deb.sh
+++ build-deb.sh
@@ -83,7 +83,9 @@ EOF
 cp -f $control.tmp $control
 
 # Modify build rules
-sed -r -i 's/flavours = .+$/flavours = blissos/g;s/test-deb/test-blissos/g' debian/rules
+sed -ri 's/flavours = .+$/flavours = blissos/g' debian/rules
+sed -ri 's/test-deb/test-blissos/g' debian/rules
+sed -ri 's/(\s*dh_install(init|systemd))/# \1/g' debian/rules
 
 # Create .orig tarball
 tar -cJf ../${pkgname}_${pkgver}.orig.tar.xz .
