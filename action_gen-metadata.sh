66fa246e5413db3c4e145307f176ba8aa82546aa
shadichy <hotrobb360web@gmail.com>
2025-01-26 22:23:46 +0700

    Added UdebVariants field to action_gen-metadata.sh script
    Signed-off-by: shadichy <hotrobb360web@gmail.com>

--- action_gen-metadata.sh
+++ action_gen-metadata.sh
@@ -5,4 +5,5 @@ Name: $(head -1 debian/changelog | awk '{print $1}')
 Version: $(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}-[0-9]+')
 Arch: $DEB_ARCH
 Variants: $(grep 'Package:' debian/control | awk '{print $2}' | xargs)
+UdebVariants: $(grep -B 1 'Package-Type: udeb' debian/control | grep 'Package:' | awk '{print $2}' | xargs)
 EOF
