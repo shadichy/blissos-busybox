ed769324ebf8504b5dc5be47a735aea32d06ae0d
ShadichyDev <74492447+ShadichyDev@users.noreply.github.com>
2025-02-02 12:33:45 +0700

    debug

--- action_build.sh
+++ action_build.sh
@@ -23,6 +23,7 @@ dependencies=""
 for p in $(dpkg-checkbuilddeps 2>&1 | grep 'Unmet build dependencies' | awk -F ':' '{print $4}'); do
   case "$p" in '('* | *')') ;; *) dependencies="$dependencies $p:$DEB_ARCH" ;; esac
 done
+echo $dependencies
 yes | apt install -y $dependencies || :
 
 export DEBEMAIL DEBFULLNAME DEB_BUILD_OPTIONS DEB_BUILD_PROFILES
