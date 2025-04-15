dbb71d61eda7de589eb7c9ed2bd79da21e78e236
Shadichy <60534636+shadichy@users.noreply.github.com>
2025-04-15 15:33:39 +0700

    fix versioning issue

--- action_gen-metadata.sh
+++ action_gen-metadata.sh
@@ -2,7 +2,7 @@
 
 cat <<EOF > metadata.yml
 Name: $(head -1 debian/changelog | awk '{print $1}')
-Version: $(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){2,}-[0-9]+')
+Version: $(head -1 debian/changelog | grep -Eo '[0-9]+(\.[0-9]+){1,}-[0-9]+(\.[0-9]+){0,}')
 Arch: $DEB_ARCH
 Variants: $(grep 'Package:' debian/control | awk '{print $2}' | xargs)
 UdebVariants: $(grep -B 1 'Package-Type: udeb' debian/control | grep 'Package:' | awk '{print $2}' | xargs)
